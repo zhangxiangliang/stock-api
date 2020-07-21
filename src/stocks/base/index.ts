5e43bf78d0621edc99ce8d74f9400fdbdbbe6214
zhangxiangliang <admin@pushme.top>
2020-07-22 01:06:07 +0800

    feat: 增加 股票接口调用基础结构

---
+++ src/stocks/base/index.ts
@@ -0,0 +1,33 @@
+// Stocks
+import BaseTransform from "@stocks/base/transform";
+
+// Stock
+import Stock from "@interfaces/Stock";
+
+/**
+ * 基础股票代码接口
+ */
+class Base {
+  /**
+   * 构造函数
+   */
+  constructor(public transform: BaseTransform) { }
+
+  /**
+   * 获取股票数据
+   * @param code 需要获取的股票代码
+   */
+  async getStock(code: string): Promise<Stock> {
+    throw new Error("未实现获取股票数据");
+  }
+
+  /**
+   * 获取股票组数据
+   * @param codes 需要获取的股票组代码
+   */
+  async getStocks(codes: string[]): Promise<Stock[]> {
+    throw new Error("未实现获取股票组数据");
+  }
+}
+
+export default Base;
