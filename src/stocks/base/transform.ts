3a32533d22e69b771d730c1625c32bc3e1704ef5
zhangxiangliang <admin@pushme.top>
2020-07-22 01:05:17 +0800

    feat: 增加 交易所代码转换 功能、根据配置的别名修改引入方式

--- src/stocks/base/transform.ts
+++ src/stocks/base/transform.ts
@@ -1,4 +1,5 @@
-import { SZ, SH, HK, US } from "../../utils/constant";
+// Utils
+import { SZ, SH, HK, US } from "@utils/constant";
 
 /**
  * 基础股票代码转换
@@ -7,7 +8,39 @@ class BaseTransform {
   /**
    * 构造函数
    */
-  constructor() {}
+  constructor() { }
+
+  /**
+   * 交易所股票代码转换
+   * @param code 股票代码
+   */
+  public transform(code: string): string {
+    if (code.includes(SH)) {
+      return this.SHExchangeTransform(code);
+    }
+
+    if (code.includes(SZ)) {
+      return this.SZExchangeTransform(code);
+    }
+
+    if (code.includes(HK)) {
+      return this.HKExchangeTransform(code);
+    }
+
+    if (code.search(US) !== -1) {
+      return this.USExchangeTransform(code);
+    }
+
+    throw new Error("请检查股票代码是否正确");
+  }
+
+  /**
+   * 交易所股票组代码转换
+   * @param codes 股票代码
+   */
+  public transforms(codes: string[]): string[] {
+    return codes.map(code => this.transform(code));
+  }
 
   /**
    * 深交所股票代码转换
--- src/stocks/netease/transform.ts
+++ src/stocks/netease/transform.ts
@@ -1,5 +1,8 @@
-import BaseTransform from "../base/transform";
-import { SZ, HK, US, SH } from "../../utils/constant";
+// Stocks
+import BaseTransform from "@stocks/base/transform";
+
+// Utils
+import { SZ, HK, US, SH } from "@utils/constant";
 
 /**
  * 网易股票代码转换
