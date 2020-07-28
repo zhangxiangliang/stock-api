5f5c18800865268ffe04f1fb1e2a21c9c61c15e2
zhangxiangliang <admin@pushme.top>
2020-07-29 02:31:50 +0800

    feat: 增加 新浪股票接口

---
+++ src/stocks/sina/api.ts
@@ -0,0 +1,78 @@
+// Stocks
+import Base from "../../stocks/base/api";
+import SinaDataTransform from "../../stocks/sina/dataTransform";
+import SinaExchangeTransform from "../../stocks/sina/exchangeTransform";
+
+// Utils
+import fetch from "../../utils/fetch";
+
+// Types
+import Stock from "../../../types/stock";
+
+/**
+ * 网易股票代码接口
+ */
+class Sina extends Base {
+  /**
+   * 构造函数
+   */
+  constructor() {
+    super();
+  }
+
+  /**
+   * 获取股票数据
+   * @param code 需要获取的股票代码
+   */
+  async getStock(code: string): Promise<Stock> {
+    const transform = (new SinaExchangeTransform).transform(code);
+
+    // 数据获取
+    const url = `https://hq.sinajs.cn/list=${transform}`;
+    const res = await fetch.get(url);
+
+    const rows = res.data.split(";\n");
+    const row = rows[0];
+
+    // 数据深解析
+    const [_, paramsUnformat] = row.split('=');
+    const params = paramsUnformat.replace('"', '').split(",");
+    const data = (new SinaDataTransform(code, params));
+
+    return {
+      code: code,
+      name: data.getName(),
+      price: data.getPrice(),
+      percent: data.getPercent(),
+    };
+  }
+
+  /**
+   * 获取股票组数据
+   * @param codes 需要获取的股票组代码
+   */
+  async getStocks(codes: string[]): Promise<Stock[]> {
+    const transforms = (new SinaExchangeTransform).transforms(codes);
+
+    // 数据获取
+    const url = `https://hq.sinajs.cn/list=${transforms.join(',')}`;
+    const res = await fetch.get(url);
+    const rows: string[] = res.data.split(";\n");
+
+    return codes.map((code, index) => {
+      // 数据深解析
+      const [_, paramsUnformat] = rows[index].split('=');
+      const params = paramsUnformat.replace('"', '').split(",");
+      const data = (new SinaDataTransform(code, params));
+
+      return {
+        code: code,
+        name: data.getName(),
+        price: data.getPrice(),
+        percent: data.getPercent(),
+      };
+    })
+  }
+}
+
+export default Sina;
---
+++ src/stocks/sina/dataTransform.ts
@@ -0,0 +1,79 @@
+// Stocks
+import Base from "../base/api";
+import SinaTransform from "./exchangeTransform";
+
+// Utils
+import { SZ, HK, US, SH } from "../../utils/constant";
+
+/**
+ * 网易数据解析
+ */
+class SinaDataTransform {
+  /**
+   * 构造函数
+   */
+  constructor(public code: string, public params: string[]) { }
+
+  /**
+   * 获取名称
+   */
+  getName(): string {
+    switch (this.code.slice(0, 2)) {
+      case SH:
+        return String(this.params[0]);
+      case SZ:
+        return String(this.params[0]);
+      case HK:
+        return String(this.params[1]);
+      case US:
+        return String(this.params[0]);
+      default:
+        throw new Error('不支持当前交易所');
+    }
+  }
+
+  /**
+   * 获取现价
+   */
+  getPrice(): number {
+    switch (this.code.slice(0, 2)) {
+      case SH:
+        return Number(this.params[3]);
+      case SZ:
+        return Number(this.params[3]);
+      case HK:
+        return Number(this.params[6]);
+      case US:
+        return Number(this.params[1]);
+      default:
+        throw new Error('不支持当前交易所');
+    }
+  }
+
+  /**
+   * 获取昨日收盘价
+   */
+  getYesterday(): number {
+    switch (this.code.slice(0, 2)) {
+      case SH:
+        return Number(this.params[2]);
+      case SZ:
+        return Number(this.params[2]);
+      case HK:
+        return Number(this.params[3]);
+      case US:
+        return Number(this.params[26]);
+      default:
+        throw new Error('不支持当前交易所');
+    }
+  }
+
+  /**
+   * 获取涨跌
+   */
+  getPercent(): number {
+    return this.getPrice() ? this.getPrice() / this.getYesterday() - 1 : 0;
+  }
+}
+
+export default SinaDataTransform;
