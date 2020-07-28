6607bc669208baa653758aa7b4e4a152340f2d3f
zhangxiangliang <admin@pushme.top>
2020-07-29 01:16:14 +0800

    feat: 修改 交易所转换器测试用例命名规则、修改 新交易所转换器导入路径

--- test/stocks/base/exchangeTransform.test.ts
+++ test/stocks/base/exchangeTransform.test.ts
@@ -1,71 +1,71 @@
 // Stock
-import BaseTransform from "../../../src/stocks/base/transform";
+import BaseExchangeTransform from "../../../src/stocks/base/exchangeTransform";
 
 describe("【基础】股票代码转换测试", () => {
   it("深交所股票代码转换", async () => {
-    expect(() => (new BaseTransform()).SZExchangeTransform("000000"))
+    expect(() => (new BaseExchangeTransform()).SZExchangeTransform("000000"))
       .toThrow(new Error("请检查股票代码是否正确"));
 
-    expect(() => (new BaseTransform()).SZExchangeTransform("SZ000000"))
+    expect(() => (new BaseExchangeTransform()).SZExchangeTransform("SZ000000"))
       .toThrow(new Error("未实现深交所股票代码转换"));
   });
 
   it("上交所股票代码转换", async () => {
-    expect(() => (new BaseTransform()).SHExchangeTransform("000000"))
+    expect(() => (new BaseExchangeTransform()).SHExchangeTransform("000000"))
       .toThrow(new Error("请检查股票代码是否正确"));
 
-    expect(() => (new BaseTransform())
+    expect(() => (new BaseExchangeTransform())
       .SHExchangeTransform("SH000000"))
       .toThrow(new Error("未实现上交所股票代码转换"));
   });
 
   it("港交所股票代码转换", async () => {
-    expect(() => new BaseTransform().HKExchangeTransform("000000"))
+    expect(() => new BaseExchangeTransform().HKExchangeTransform("000000"))
       .toThrow(new Error("请检查股票代码是否正确"));
 
-    expect(() => new BaseTransform().HKExchangeTransform("HK000000"))
+    expect(() => new BaseExchangeTransform().HKExchangeTransform("HK000000"))
       .toThrow(new Error("未实现港交所股票代码转换"));
   });
 
   it("美交所股票代码转换", async () => {
-    expect(() => (new BaseTransform()).USExchangeTransform("000000"))
+    expect(() => (new BaseExchangeTransform()).USExchangeTransform("000000"))
       .toThrow(new Error("请检查股票代码是否正确"));
 
-    expect(() => (new BaseTransform()).USExchangeTransform("US000000"))
+    expect(() => (new BaseExchangeTransform()).USExchangeTransform("US000000"))
       .toThrow(new Error("未实现美交所股票代码转换"));
   });
 
   it("交易所股票代码转换", async () => {
-    expect(() => (new BaseTransform()).transform("000000"))
+    expect(() => (new BaseExchangeTransform()).transform("000000"))
       .toThrow(new Error("请检查股票代码是否正确"));
 
-    expect(() => (new BaseTransform()).transform("SZ000000"))
+    expect(() => (new BaseExchangeTransform()).transform("SZ000000"))
       .toThrow(new Error("未实现深交所股票代码转换"));
 
-    expect(() => (new BaseTransform()).transform("SH000000"))
+    expect(() => (new BaseExchangeTransform()).transform("SH000000"))
       .toThrow(new Error("未实现上交所股票代码转换"));
 
-    expect(() => (new BaseTransform()).transform("HK000000"))
+    expect(() => (new BaseExchangeTransform()).transform("HK000000"))
       .toThrow(new Error("未实现港交所股票代码转换"));
 
-    expect(() => (new BaseTransform()).transform("US000000"))
+    expect(() => (new BaseExchangeTransform()).transform("US000000"))
       .toThrow(new Error("未实现美交所股票代码转换"));
   });
 
   it("交易所股票组代码转换", async () => {
-    expect(() => (new BaseTransform()).transforms(["000000"]))
+    expect(() => (new BaseExchangeTransform()).transforms(["000000"]))
       .toThrow(new Error("请检查股票代码是否正确"));
 
-    expect(() => (new BaseTransform()).transforms(["SZ000000"]))
+    expect(() => (new BaseExchangeTransform()).transforms(["SZ000000"]))
       .toThrow(new Error("未实现深交所股票代码转换"));
 
-    expect(() => (new BaseTransform()).transforms(["SH000000"]))
+    expect(() => (new BaseExchangeTransform()).transforms(["SH000000"]))
       .toThrow(new Error("未实现上交所股票代码转换"));
 
-    expect(() => (new BaseTransform()).transforms(["HK000000"]))
+    expect(() => (new BaseExchangeTransform()).transforms(["HK000000"]))
       .toThrow(new Error("未实现港交所股票代码转换"));
 
-    expect(() => (new BaseTransform()).transforms(["US000000"]))
+    expect(() => (new BaseExchangeTransform()).transforms(["US000000"]))
       .toThrow(new Error("未实现美交所股票代码转换"));
   });
 });
