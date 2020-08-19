4471092aabe0fd9eb72a93cb391aea4ea6925f8c
zhangxiangliang <admin@pushme.top>
2020-08-19 23:29:18 +0800

    style: 修改 基础统一代码转换股票代码文案

--- src/stocks/base/transforms/common-code.ts
+++ src/stocks/base/transforms/common-code.ts
@@ -15,7 +15,7 @@ class BaseCommonCodeTransform implements CommonCodeTransform {
 
   /**
    * 交易所统一代码转换股票代码
-   * @param code 股票代码
+   * @param code 统一代码
    */
   public transform(code: string): string {
     if (code.includes(COMMON_SH)) {
@@ -38,7 +38,7 @@ class BaseCommonCodeTransform implements CommonCodeTransform {
   }
 
   /**
-   * 交易所股票组代码转换
+   * 交易所统一代码组转股票代码组
    * @param codes 股票代码
    */
   public transforms(codes: string[]): string[] {
@@ -47,7 +47,7 @@ class BaseCommonCodeTransform implements CommonCodeTransform {
 
   /**
    * 深交所统一代码转换股票代码
-   * @param code 股票代码
+   * @param code 统一代码
    */
   public SZTransform(code: string): string {
     if (!code.includes(COMMON_SZ)) {
@@ -59,7 +59,7 @@ class BaseCommonCodeTransform implements CommonCodeTransform {
 
   /**
    * 上交所统一代码转换股票代码
-   * @param code 股票代码
+   * @param code 统一代码
    */
   public SHTransform(code: string): string {
     if (!code.includes(COMMON_SH)) {
@@ -71,7 +71,7 @@ class BaseCommonCodeTransform implements CommonCodeTransform {
 
   /**
    * 港交所统一代码转换股票代码
-   * @param code 股票代码
+   * @param code 统一代码
    */
   public HKTransform(code: string): string {
     if (!code.includes(COMMON_HK)) {
@@ -83,7 +83,7 @@ class BaseCommonCodeTransform implements CommonCodeTransform {
 
   /**
    * 美交所统一代码转换股票代码
-   * @param code 股票代码
+   * @param code 统一代码
    */
   public USTransform(code: string): string {
     if (!code.includes(COMMON_US)) {
