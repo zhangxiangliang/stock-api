1f96a91c39b6093794d071d4236639c78429d5e1
zhangxiangliang <admin@pushme.top>
2020-08-20 23:19:58 +0800

    feat: 增加 默认股票数据常量

--- src/stocks/base/utils/constant.ts
+++ src/stocks/base/utils/constant.ts
@@ -2,3 +2,15 @@ export const COMMON_SZ = "SZ"; // 深交所相关常量
 export const COMMON_SH = "SH"; // 上交所相关常量
 export const COMMON_HK = "HK"; // 港交所相关常量
 export const COMMON_US = "US"; // 美交所相关常量
+
+// 默认股票数据常量
+export const DEFAULT_STOCK = {
+  code: '---',
+  name: '---',
+  percent: 0,
+
+  now: 0,
+  low: 0,
+  high: 0,
+  yesterday: 0,
+};
